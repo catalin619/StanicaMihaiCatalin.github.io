3602207b141db79cba6db6d59a0552326eec95e9
catalin619 <56998762+catalin619@users.noreply.github.com>
2019-10-25 13:42:33 +0300

    Add files via upload

--- proiect mobile/gyro.js
+++ proiect mobile/gyro.js
@@ -1,8 +1,18 @@
-window.addEventListener("deciceorienation", on_roate_uab);
+document.getElementsByClassName("id_logic_version").innerHTML = "Logic version = 2019.10.25.0"
+window.addEventListener("deciceorienation", on_orentation_uab);
+window.addEventListener("deciceorienation", on_motion_uab);
 
-function on_rotate_uab(e)
+function on_orentation_uab(e)
 {
   document.getElementById("id_alpha").innerHTML = e.alpha;
   document.getElementById("id_beta").innerHTML = e.beta;
   document.getElementById("id_gamma").innerHTML = e.gamma;
+
+}
+
+function on_montion_uab(e)
+{
+  document.getElementById("id_acc_x").innerHTML = e.accelerationIncludingGravity.x;
+  document.getElementById("id_acc_y").innerHTML = e.accelerationIncludingGravity.y;
+  document.getElementById("id_acc_z").innerHTML = e.accelerationIncludingGravity.z;
 }
